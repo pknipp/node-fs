cbc0977b286a8c50702cdf2cbf632d838f642868
Peter Knipp <peteraknipp@gmail.com>
2021-06-08 22:26:16 -0400

    Implement session/delete route.

--- client/src/components/admin/Logout.js
+++ client/src/components/admin/Logout.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-// import { Button } from '@material-ui/core';
-import { logout } from './store/authentication';
+import AuthContext from '../../auth';
 
-class Logout extends Component {
+const Logout = () => {
+  const { fetchWithCSRF, currentUser, setCurrentUser } = useContext(AuthContext);
 
-  handleSubmit = e => {
+  const logout = async () => {
+    const res = await fetch('/api/session', { method: "delete" });
+    if (res.ok) setCurrentUser(null);
+  }
+
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.logout();
+    logout();
   }
 
-  render() {
-    return (!this.props.currentUserId) ? <Redirect to="/login" /> : (
-      <form className="simple" onSubmit={this.handleSubmit}>
-        <button color="primary" variant="outlined" type="submit">Logout</button>
-      </form>
-    );
-  }
+  return (!currentUser) ? <Redirect to="/login" /> : (
+    <form className="simple" onSubmit={handleSubmit}>
+      <button color="primary" variant="outlined" type="submit">Logout</button>
+    </form>
+  );
 }
 
-const msp = state => ({ currentUserId: state.authentication.id });
-const mdp = dispatch => ({ logout: () => dispatch(logout())})
-export default connect(msp, mdp)(Logout);
+// const msp = state => ({ currentUserId: state.authentication.id });
+// const mdp = dispatch => ({ logout: () => dispatch(logout())})
+export default Logout;
